17ff7b64d98817305aed6c62b4585e19ed763544
tom <ychen306@mit.edu>
2022-11-14 18:08:53 -0500

    add simple match test; passing

--- tests.cpp
+++ tests.cpp
@@ -112,4 +112,53 @@ TEST(PatternTest, make) {
 }
 
 TEST(MatchTest, simple) {
+  auto px = Pattern::var();
+  auto py = Pattern::var();
+  auto pf = Pattern::make(42, {px, py});
+
+  EGraph g;
+  auto x = g.make(0);
+  auto y = g.make(1);
+  auto f = g.make(42, {x, y});
+  auto matches = match(pf.get(), g);
+  ASSERT_EQ(matches.size(), 1);
+
+  auto &m = matches.front();
+  llvm::DenseMap<Pattern *, EClass *> subst(m.begin(), m.end());
+  ASSERT_EQ(subst[px.get()], x);
+  ASSERT_EQ(subst[py.get()], y);
+}
+
+TEST(MatchTest, simple2) {
+  auto px = Pattern::var();
+  auto py = Pattern::var();
+  auto pf = Pattern::make(42, {px, py});
+
+  EGraph g;
+  auto x = g.make(0);
+  auto y = g.make(1);
+  auto a = g.make(2);
+  auto b = g.make(3);
+  g.make(42, {x, y});
+  g.make(42, {a, b});
+  auto matches = match(pf.get(), g);
+  ASSERT_EQ(matches.size(), 2);
+
+  auto &m0 = matches[0];
+  auto &m1 = matches[1];
+
+  llvm::DenseMap<Pattern *, EClass *> subst1(m0.begin(), m0.end());
+  llvm::DenseMap<Pattern *, EClass *> subst2(m1.begin(), m1.end());
+  bool isXY = subst1[px.get()] == x;
+  if (isXY) {
+    ASSERT_EQ(subst1[px.get()], x);
+    ASSERT_EQ(subst1[py.get()], y);
+    ASSERT_EQ(subst2[px.get()], a);
+    ASSERT_EQ(subst2[py.get()], b);
+  } else {
+    ASSERT_EQ(subst2[px.get()], x);
+    ASSERT_EQ(subst2[py.get()], y);
+    ASSERT_EQ(subst1[px.get()], a);
+    ASSERT_EQ(subst1[py.get()], b);
+  }
 }
